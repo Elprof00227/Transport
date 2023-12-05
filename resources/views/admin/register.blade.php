f552eb6a5827806e28be242a76619553fcd5010e
Elprof00227 <prestatechniger@gmail.com>
2023-12-05 15:48:19 +0100

    message

---
+++ resources/views/admin/register.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+
+@section('title')
+Registre   
+@endsection
+
+@section('content')
+<div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title">Enregistrement Roles</h4>
+              </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class=" text-primary">
+                      <th>Name</th>
+                      <th>Phone</th>
+                      <th>Email</th>
+                      <th>EDIT</th>
+                      <th>DELETE</th>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Dakota Rice</td>
+                        <td>Niger</td>
+                        <td>Oud-Turnhout</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+@endsection
+@section('scripts')
+
+@endsection
